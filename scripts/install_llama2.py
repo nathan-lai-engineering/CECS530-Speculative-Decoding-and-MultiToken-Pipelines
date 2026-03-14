709c2acb68db626b58e92d7f25b4d0ea9a65cc30
Nathan Lai <42527870+nathan-lai-engineering@users.noreply.github.com>
2026-03-14 05:40:48 -0700

    model install script for consistency

--- scripts/install_llama2.py
+++ scripts/install_llama2.py
@@ -1,26 +1,26 @@
 import os
-import sys
 from pathlib import Path
 from dotenv import load_dotenv
-from huggingface_hub import HfApi, snapshot_download
+from huggingface_hub import snapshot_download
 
-load_dotenv()
-token = os.getenv("HF_TOKEN")
-if token:
-    api = HfApi()
-    model_id = "meta-llama/Llama-2-7b-hf"
-    print(api.model_info(model_id, token=token))
+def download_hf_model(model_id, output):
+    load_dotenv()
+    token = os.getenv("HF_TOKEN")
+    if token:
+        os.rmdir(output) # for clean install
 
-    output_path = Path("./models/llama2-7b")
-    output_path.mkdir(parents=True, exist_ok=True)
+        output_path = Path(output)
+        output_path.mkdir(parents=True, exist_ok=True)
 
-    path = snapshot_download(
-        repo_id=model_id,
-        token=token,
-        local_dir=str(output_path),
-        local_dir_use_symlinks=False,
-        ignore_patterns=["*.msgpack", "*.h5", "flax_model*", "tf_model*"],
-    )
-else:
-    print("download the .env file with the token or get one yourself")
+        snapshot_download(
+            repo_id=model_id,
+            token=token,
+            local_dir=str(output_path),
+            local_dir_use_symlinks=False,
+            ignore_patterns=["*.msgpack", "*.h5", "flax_model*", "tf_model*"],
+        )
+    else:
+        print("download the .env file with the token or get one yourself")
 
+download_hf_model("meta-llama/Llama-2-7b-hf", "./models/llama2-7b") #13 GB
+download_hf_model("TinyLlama/TinyLlama-1.1B-Chat-v1.0", "./models/tinyllama-1.1b") #2.2 GB
